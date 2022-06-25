1264952953547536f04bfd3d498f0fcfca7ed3ce
Ido Lourie <63515984+idolou@users.noreply.github.com>
2022-06-25 14:57:33 +0300

    style

--- src/View/Main.java
+++ src/View/Main.java
@@ -5,8 +5,12 @@ package View;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,9 +18,30 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("MyView.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.setTitle("Maze App");
+        Image image = new Image("/resources/Backround/maze.png");
+        primaryStage.getIcons().add(image);
+
+
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+
+        primaryStage.setX(bounds.getMinX());
+        primaryStage.setY(bounds.getMinY());
+        primaryStage.setWidth(bounds.getWidth());
+        primaryStage.setHeight(bounds.getHeight());
+
+//        StackPane root1 = new StackPane();
+        root.setStyle(
+                "-fx-background-image: url(" +
+                        "'/resources/Backround/cosmic.jpg'" +
+                        "); " +
+                        "-fx-background-size: cover;"
+        );
+
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
     }
 
 
